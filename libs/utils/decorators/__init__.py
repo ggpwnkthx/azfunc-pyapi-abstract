3c47c35512496f79e515a5535e0236ae07950456
Isaac Jessup <ibjessup@gmail.com>
2023-04-19 17:59:03 -0400

    immutable arguments

--- libs/utils/decorators/__init__.py
+++ libs/utils/decorators/__init__.py
@@ -1,5 +1,35 @@
 class staticproperty(object):
     def __init__(self, f):
         self.f = f
+
     def __get__(self, obj, owner):
         return self.f(owner)
+
+
+from collections.abc import Collection, Mapping, Hashable
+from frozendict import frozendict
+import functools
+
+def deep_freeze(thing):
+    if thing is None or isinstance(thing, str):
+        return thing
+    elif isinstance(thing, Mapping):
+        return frozendict({k: deep_freeze(v) for k, v in thing.items()})
+    elif isinstance(thing, Collection):
+        return tuple(deep_freeze(i) for i in thing)
+    elif not isinstance(thing, Hashable):
+        raise TypeError(f"unfreezable type: '{type(thing)}'")
+    else:
+        return thing
+    
+def immutable_arguments(func):
+    """
+    Transform mutable dictionnary into immutable
+    Useful to be compatible with cache
+    """
+
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        return func(*deep_freeze(args), **deep_freeze(kwargs))
+
+    return wrapped
