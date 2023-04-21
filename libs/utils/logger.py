36baa96d21feea8a109c060171a469a0c4518086
Isaac Jessup <ibjessup@gmail.com>
2023-04-21 13:40:53 -0400

    auto logger

--- libs/utils/logger.py
+++ libs/utils/logger.py
@@ -1,15 +1,29 @@
+from pathlib import Path
+from typing import Any
+import inspect
 import logging
-import os
 
-def get(name:str, level = logging.DEBUG ) -> logging.Logger:
+
+def get(name: str, level=logging.DEBUG) -> logging.Logger:
     # init logger object
     logger = logging.getLogger(name)
     logger.setLevel(level)
     # create file handler if it doesn't exist
     if not len(logger.handlers):
-        if not os.path.exists("logs"):
-            os.makedirs("logs")
-        fh = logging.FileHandler(f"logs/{name}.txt")
+        path = Path(f"logs/{name}.txt")
+        path.parent.mkdir(parents=True, exist_ok=True)
+        fh = logging.FileHandler(str(path))
         fh.setLevel(logging.DEBUG)
         logger.addHandler(fh)
     return logger
+
+
+def WARNING(message: Any, name: str = None, *args, **kwargs):
+    if not name:
+        frame = inspect.stack()[1]
+        module = inspect.getmodule(frame[0])
+        name = str(Path(module.__file__).relative_to(Path.cwd()))
+    if not len(args) and not len(kwargs):
+        get(name).warning(message)
+    else:
+        get(name).warning((message, *args, kwargs))
