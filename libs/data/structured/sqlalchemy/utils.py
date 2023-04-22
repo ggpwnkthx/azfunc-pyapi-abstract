1c3208d29b5930e5d223c2230c5849c3295ac4e3
Isaac Jessup <ibjessup@gmail.com>
2023-04-22 14:07:32 -0400

    bracket syntax querying

--- libs/data/structured/sqlalchemy/utils.py
+++ libs/data/structured/sqlalchemy/utils.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable
+from typing import Any, Callable, Union
 import re
 import inflect
 
@@ -57,23 +57,111 @@ def extend_model(model: Any, session: Session) -> None:
                         "model": model,
                         "sqla_session": session,
                         "render_module": simplejson,
+                        # "include_relationships": True,
+                        # "load_instance": True,
                     },
                 )
             },
         ),
     )
     setattr(model, "__repr__", lambda self: self.__marshmallow__().dumps(self))
-    setattr(model, "__json__", lambda self: simplejson.loads(str(self)))
-
-
-def model_get(session: Session) -> Callable:
-    @classmethod
-    def __class_getitem__(cls, id) -> Any:
-        return session().query(cls).get(id)
-
-    return __class_getitem__
 
 
 def model_set_column(self: object, key: str, value: Any) -> None:
     setattr(self, key, value)
     self._sa_instance_state.session.commit()
+
+
+import logging
+from sqlalchemy import Column
+from sqlalchemy.inspection import inspect
+from sqlalchemy.orm import Mapper, Query, InstrumentedAttribute
+from sqlalchemy.sql.elements import BinaryExpression, BooleanClauseList
+
+
+class QueryFrame:
+    def __init__(self, model: Any, session: Session) -> None:
+        self.model = model
+        self.mapper: Mapper = inspect(model)
+        self.session = session
+        self.select = []
+        self.ops = []
+        self.sort = []
+        self.limit = 0
+        self.offset = 0
+
+    @property
+    def __primary_key__(self) -> Column:
+        for column in self.mapper.columns.values():
+            if column.primary_key:
+                return column
+
+    def __getitem__(self, key: Any = None):
+        match key:
+            case list():
+                logging.debug(("list", type(key), key))
+                for item in key:
+                    self[item]
+            case tuple():
+                logging.debug(("tuple", type(key), key))
+                return [self[item] for item in key]
+            case InstrumentedAttribute():
+                logging.debug(("column", type(key), key))
+                self.select.append(key)
+            case BinaryExpression() | BooleanClauseList():
+                logging.debug(("expression", type(key), key))
+                self.ops.append(("where", key))
+            case str():
+                logging.debug(("string", type(key), key))
+                return self.__getitem_field(key)
+            case slice():
+                logging.debug(("slice", type(key), key))
+                self.limit = key.stop - key.start
+                self.offset = key.start
+            case _:
+                logging.debug(("other", type(key), key))
+        return self
+
+    def __build__(self) -> Query:
+        query: Query = (
+            self.session().query(*(self.select or [self.model])).select_from(self.model)
+        )
+        for op in self.ops:
+            query = getattr(query, op[0])(op[1])
+        return query
+
+    def __call__(self):
+        return self.__build__().all()
+    
+    def __len__(self) -> int:
+        return self.__build__().count()
+
+    def __repr__(self) -> str:
+        try:
+            from sql_formatter.core import format_sql
+            return format_sql(str(self.__build__()))
+        except:
+            return str(self.__build__())
+
+    def __getitem_id(Self, key):
+        logging.warn(("id", type(key), key))
+
+    def __getitem_field(self, key):
+        if isinstance(field := getattr(self.model, key, None), InstrumentedAttribute):
+            return field
+    
+    def to_pandas(self):
+        try:
+            import pandas as pd
+        except:
+            raise Exception("Pandas is not installed.")
+        return pd.read_sql(str(self.__build__()), self.session().connection())
+
+
+def model_get(session: Session) -> Callable:
+    @classmethod
+    def __class_getitem__(cls, key) -> Union[Query, Any]:
+        frame = QueryFrame(cls, session)
+        return frame[key]
+
+    return __class_getitem__
