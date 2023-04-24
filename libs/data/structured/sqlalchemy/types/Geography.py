8ee867c7b4f303747beb4632c1c036a103585a71
Isaac B. Jessup <ibjessup@gmail.com>
2023-04-23 20:45:01 -0400

    GeoJSON working

--- libs/data/structured/sqlalchemy/types/Geography.py
+++ libs/data/structured/sqlalchemy/types/Geography.py
@@ -1,63 +1,58 @@
-# from libs.utils.geometry import wkt2geojson, geojson2wkt
-# from geojson import GeoJSON
-# from sqlalchemy import func, UserDefinedType, JSON
-# from sqlalchemy.sql.expression import text
-# from sqlalchemy.sql.schema import Column
-# from sqlalchemy.types import TypeEngine
+from marshmallow.fields import Nested
+from marshmallow_geojson import GeometryCollectionSchema, GeoJSONSchema
+from marshmallow_sqlalchemy.convert import ModelConverter
+from shapely.geometry import GeometryCollection
+from sqlalchemy.dialects.mssql.base import ischema_names as mssql_ischema_names
+import geoalchemy2.types
+import geojson
+import shapely.wkb
+
+import logging
 
 
-# class Geography(UserDefinedType):
-#     cache_ok = True
-
-#     __visit_name__ = "Geography"
-
-#     def __init__(self, srid: int = 4326):
-#         self.srid = srid
-
-#     def get_col_spec(self):
-#         return self.name
-
-#     def bind_expression(self, bindvalue):
-#         # not able to use func here since "::" in function name
-#         exp = f"geography::STGeomFromText(:{bindvalue.key}, {self.srid})"
-#         f = text(exp)
-#         return f.bindparams(bindvalue)
-
-#     def column_expression(self, col: Column):
-#         # not able to use func here since function needs to be applied as method to the column itself
-#         # col.func() not func(col)
-#         exp = f"{col.key}.STAsText()"
-#         f1 = text(exp)
-#         f2 = func.IIF(col == None, None, f1, type_=self)
-#         return f2
-
+# class Geometry(geoalchemy2.types.Geometry):
 #     def result_processor(self, dialect, coltype):
 #         def process(value):
-#             return wkt2geojson(value) if value else value
+#             wkb = shapely.wkb.loads(value) if value else value
+#             # match cls := getattr(geojson,shape.type, None):
+#             #     case geojson.GeometryCollection:
+#             #         return cls(geometries=[geojson.Feature(geometry=o) for o in shape.geoms])
+#             #     case _:
+#             #         return geojson.Feature(geometry=shape)
+#             return geojson.GeometryCollection(
+#                 geometries=wkb.geoms if type(wkb) == GeometryCollection else [wkb]
+#             )
 
 #         return process
 
 #     def bind_processor(self, dialect):
 #         def process(value):
-#             return geojson2wkt(value) if value else value
+#             return shapely.wkb.loads(value) if value else value
 
 #         return process
 
-#     class comparator_factory(TypeEngine.Comparator):
-#         def __add__(self, other):
-#             return self.op("goofy")(other)
 
-#         def intersects_bounds(self, lon_min, lon_max, lat_min, lat_max):
-#             p = f"POLYGON (({lon_min} {lat_min}, {lon_max} {lat_min}, {lon_max} {lat_max}, {lon_min} {lat_max}, {lon_min} {lat_min}))"
-#             exp = f"geography::STGeomFromText('{p}', {self.type.srid}).STIntersects({self.expr.name}) = 1"
-#             f = text(exp)
-#             return f
+# class Geography(Geometry):
+#     pass
 
-from geoalchemy2.types import Geometry as Gm, Geography as Gg
+
+# mssql_ischema_names["geometry"] = Geometry
+# mssql_ischema_names["geography"] = Geography
+
+
+# class GeometryField(Nested):
+#     def __init__(self, **kwargs):
+#         super().__init__(GeometryCollectionSchema, **kwargs)
+
+
+# ModelConverter.SQLA_TYPE_MAPPING[Geometry] = GeometryField
+# ModelConverter.SQLA_TYPE_MAPPING[Geography] = GeometryField
+
+
 from libs.utils.geometry import wkb2geojson, geojson2wkb
-from sqlalchemy.dialects.mssql.base import ischema_names as mssql_ischema_names
 
-class Geography(Gg):
+
+class GeometryJSON(geoalchemy2.types.Geometry):
     def result_processor(self, dialect, coltype):
         def process(value):
             return wkb2geojson(value) if value else value
@@ -70,16 +65,19 @@ class Geography(Gg):
 
         return process
 
-# mssql_ischema_names["geometry"] = Geometry
-mssql_ischema_names["geography"] = Geography
 
-from marshmallow.fields import Nested
-from marshmallow_geojson import GeometriesSchema, GeoJSONSchema
-from marshmallow_sqlalchemy.convert import ModelConverter
+class GeographyJSON(GeometryJSON):
+    pass
 
-class Geometry(Nested):
+
+mssql_ischema_names["geometry"] = GeometryJSON
+mssql_ischema_names["geography"] = GeographyJSON
+
+
+class GeoJSONField(Nested):
     def __init__(self, **kwargs):
         super().__init__(GeoJSONSchema, **kwargs)
 
-ModelConverter.SQLA_TYPE_MAPPING[Geometry] = Geometry
-ModelConverter.SQLA_TYPE_MAPPING[Geography] = Geometry
+
+ModelConverter.SQLA_TYPE_MAPPING[GeometryJSON] = GeoJSONField
+ModelConverter.SQLA_TYPE_MAPPING[GeographyJSON] = GeoJSONField
