d34e6226fbf5197d49355e5ab5f4ef33af98f560
Isaac Jessup <ibjessup@gmail.com>
2023-05-19 20:25:04 -0400

    init helper
    modular code, can be used in other triggers now (not just http)

---
+++ libs/azure/functions/blueprints/onspot/http/starter.py
@@ -0,0 +1,16 @@
+from ..helpers import onspot_initializer
+from azure.durable_functions import DurableOrchestrationClient
+from libs.azure.functions import Blueprint
+from libs.azure.functions.http import HttpRequest
+
+bp = Blueprint()
+
+
+# An HTTP-Triggered Function with a Durable Functions Client binding
+@bp.route(route="onspot/{*endpoint}", methods=["POST"])
+@bp.durable_client_input(client_name="client")
+async def onspot_starter(req: HttpRequest, client: DurableOrchestrationClient):
+    # Generate an instance ID
+    instanceId = await onspot_initializer(req=req, client=client)
+    response = client.create_check_status_response(request=req, instance_id=instanceId)
+    return response
