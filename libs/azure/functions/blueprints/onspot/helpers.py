5c1d78a59dc99df4cd562fc23dcef0b6b44c831b
Isaac Jessup <ibjessup@gmail.com>
2023-05-23 17:25:57 -0400

    onspot scheduling

--- libs/azure/functions/blueprints/onspot/helpers.py
+++ libs/azure/functions/blueprints/onspot/helpers.py
@@ -1,9 +1,15 @@
 from azure.durable_functions import DurableOrchestrationClient, EntityId
-from libs.azure.functions.http import HttpRequest
 from libs.onspot.endpoints import REGISTRY, EndpointToStorage
 import uuid
 
-async def onspot_initializer(req: HttpRequest, client: DurableOrchestrationClient):
+
+async def onspot_initializer(
+    endpoint: str,
+    request: dict,
+    client: DurableOrchestrationClient,
+    context: dict = None,
+    response_url: str = None,
+):
     # Generate an instance ID
     instanceId = str(uuid.uuid4())
 
@@ -12,16 +18,16 @@ async def onspot_initializer(req: HttpRequest, client: DurableOrchestrationClien
         entityId=EntityId(name="entity_generic", key=f"{instanceId}_settings"),
         operation_name="init",
         operation_input={
-            "endpoint": req.route_params["endpoint"],
-            "context": {key: value for key, value in req.params.items()},
-            "target": (
-                "blobs"
-                if issubclass(REGISTRY[req.route_params["endpoint"]], EndpointToStorage)
-                else "callback"
-            ),
-            "response_links": client.get_client_response_links(
-                request=req, instance_id=instanceId
-            ),
+            "endpoint": endpoint,
+            "context": context,
+            "links": {
+                k: client._replace_url_origin(response_url, v)
+                if response_url
+                else v.replace(
+                    client._orchestration_bindings.management_urls["id"], instanceId
+                )
+                for k, v in client._orchestration_bindings.management_urls.copy().items()
+            },
         },
     )
 
@@ -29,12 +35,12 @@ async def onspot_initializer(req: HttpRequest, client: DurableOrchestrationClien
     await client.signal_entity(
         entityId=EntityId(name="entity_generic", key=f"{instanceId}_data"),
         operation_name="init",
-        operation_input=req.get_json(),
+        operation_input=request,
     )
 
     # Start orchestrator
     await client.start_new(
         orchestration_function_name="onspot_orchestrator", instance_id=instanceId
     )
-    
+
     return instanceId
--- libs/azure/functions/blueprints/onspot/starters/scheduler/starter.py
+++ libs/azure/functions/blueprints/onspot/starters/scheduler/starter.py
@@ -1,6 +1,6 @@
 from azure.data.tables import TableClient
 from azure.durable_functions import DurableOrchestrationClient
-from azure.functions import TimerRequest
+from azure.functions import TimerRequest, Context
 from datetime import datetime
 from libs.azure.functions import Blueprint
 from libs.azure.functions.blueprints.onspot.config import (
@@ -18,9 +18,11 @@ bp = Blueprint()
 # Daily timer trigger
 @bp.timer_trigger(arg_name="timer", schedule="0 0 0 * * *")
 @bp.durable_client_input(client_name="client")
+@bp.logger()
 async def onspot_starter_scheduler(
-    timer: TimerRequest, client: DurableOrchestrationClient
+    timer: TimerRequest, client: DurableOrchestrationClient, context: Context
 ):
+    print({k:getattr(timer,k) for k in dir(timer) if not k.startswith("_")})
     job_schema = JobSchema()
     jobs_table = TableClient.from_connection_string(
         conn_str=ONSPOT_CONN_STR, table_name=ONSPOT_TABLE_NAMES["jobs"]
@@ -33,12 +35,14 @@ async def onspot_starter_scheduler(
         logging.warn(job)
         init = {
             "endpoint": job["EndPoint"],
-            "request": {"features": [
-                e["Value"]
-                for e in config_table.query_entities(
-                    f"PartitionKey eq '{job['PartitionKey']}' and JobKey eq '{job['RowKey']}' and Type eq 'Feature'"
-                )
-            ]}
+            "request": {
+                "features": [
+                    e["Value"]
+                    for e in config_table.query_entities(
+                        f"PartitionKey eq '{job['PartitionKey']}' and JobKey eq '{job['RowKey']}' and Type eq 'Feature'"
+                    )
+                ]
+            }
             if job["EndPoint"].startswith("geoframe")
             or job["EndPoint"].startswith("save/geoframe")
             else {},
