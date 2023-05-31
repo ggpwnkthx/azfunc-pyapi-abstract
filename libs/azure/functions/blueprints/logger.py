ce0008aa2e66d5f91f69c39c0f9892cc9c325ab9
Isaac Jessup <ibjessup@gmail.com>
2023-05-31 09:40:25 -0400

    logger with context

--- libs/azure/functions/blueprints/logger.py
+++ libs/azure/functions/blueprints/logger.py
@@ -1,13 +1,21 @@
+from azure.functions import Context
+from azure.storage.blob import BlobClient
 from libs.azure.functions import Blueprint
 from libs.azure.functions.http import HttpRequest, HttpResponse
-
+import os
 import logging
 
 bp = Blueprint()
 
 
 @bp.route(route="logger", methods=["POST"])
-async def logger(req: HttpRequest):
+async def logger(req: HttpRequest, context: Context):
     data = req.get_body()
+    BlobClient.from_connection_string(
+        conn_str=os.environ["AzureWebJobsStorage"],
+        container_name="general",
+        blob_name=context.invocation_id,
+    ).upload_blob(data)
+    logging.warn(dict(req.params))
     logging.warn(data)
     return HttpResponse("OK")
