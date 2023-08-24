225dd90e0e49b13ee623a188e6585b9215340791
Isaac Jessup <ibjessup@gmail.com>
2023-08-24 13:14:58 -0400

    openapi xandr

--- libs/openapi/clients/xandr/__init__.py
+++ libs/openapi/clients/xandr/__init__.py
@@ -1,8 +1,23 @@
-from aiopenapi3 import OpenAPI
+from aiopenapi3 import OpenAPI, ResponseDecodingError
+from aiopenapi3.plugin import Message
+from io import BytesIO
 import httpx, pathlib, os, yaml
+import pandas as pd
+
+
+class XandrReportFormatter(Message):
+    def received(self, ctx: "Message.Context") -> "Message.Context":
+        if ctx.operationId.startswith("Download"):
+            try:
+                ctx.received = pd.read_csv(BytesIO(ctx.received)).to_dict()
+            except Exception as e:
+                raise ResponseDecodingError(ctx.received, None, None) from e
+            return ctx
 
 
 class XandrAPI:
+    api_key = None
+    
     def __new__(
         cls,
         api_key: str = os.environ.get(
@@ -20,9 +35,13 @@ class XandrAPI:
             url=f"https://api.appnexus.com",
             document=XandrAPI.get_spec(),
             session_factory=httpx.AsyncClient if asynchronus else httpx.Client,
+            plugins=[XandrReportFormatter()],
+            use_operation_tags=False,
         )
+        if cls.api_key and not api_key:
+            api_key = cls.api_key
         if not api_key and username and password:
-            api_key = cls.get_token(username, password)
+            api_key = cls.api_key = cls.get_token(username, password)
         api.authenticate(
             token=api_key,
         )
@@ -39,7 +58,7 @@ class XandrAPI:
     ):
         api = OpenAPI(
             url=f"https://api.appnexus.com",
-            document=XandrAPI.spec,
+            document=XandrAPI.get_spec(),
             session_factory=httpx.Client,
         )
         auth = api.createRequest(("/auth", "post"))
