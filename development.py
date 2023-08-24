0fac402335eb5cce163c5564de6a8baddc778b48
Isaac Jessup <ibjessup@gmail.com>
2023-08-24 13:12:12 -0400

    auto bp selector
    Automatically registers blueprints based on the deployment's site name.

---
+++ development.py
@@ -0,0 +1,9 @@
+BLUEPRINTS = [
+    # "libs/azure/functions/blueprints/onspot/*",
+    # "libs/azure/functions/blueprints/esquire/dashboard/*",
+    "libs/azure/functions/blueprints/esquire/dashboard/xandr/*",
+    "libs/azure/functions/blueprints/esquire/dashboard/meta/*",
+    "libs/azure/functions/blueprints/meta/*",
+    "libs/azure/functions/blueprints/datalake/*",
+    "libs/azure/functions/blueprints/synapse/*",
+]
--- function_app.py
+++ function_app.py
@@ -1,21 +1,7 @@
 from azure.functions import AuthLevel
 from libs.azure.functions import FunctionApp
+from deployment import get_bps
 
 app = FunctionApp(http_auth_level=AuthLevel.ANONYMOUS)
 
-app.register_blueprints(
-    [
-        # "libs/azure/functions/blueprints/keepalive",
-        # "libs/azure/functions/blueprints/logger",
-        "libs/azure/functions/blueprints/docs/*",
-        
-        ## esquire-oneview-tasks
-        # "libs/azure/functions/blueprints/oneview/tasks/*",
-        
-        # esquire-dashboard-data
-        # "libs/azure/functions/blueprints/datalake/*",
-        # "libs/azure/functions/blueprints/synapse/*",
-        # "libs/azure/functions/blueprints/onspot/*",
-        # "libs/azure/functions/blueprints/esquire/dashboard/onspot/*",
-    ]
-)
+app.register_blueprints(get_bps(debug=False))
